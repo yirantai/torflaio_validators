9cc17ccabf391208a6b30a951c73d3066ccf8903
yirantai <896275756@qq.com>
2022-07-07 17:14:14 +0800

    [Fix]:upgrade Integer default

--- base/fields.py
+++ base/fields.py
@@ -19,29 +19,32 @@ class BaseField(object):
         self.min_length = min_length
 
     def integrity_validate(self):
-        """参数完整性验证"""
+        """Parameter integrity verification"""
         if self.required and self.field_value is None:
-            raise ParamsValidationError('参数错误, 字段%s为必填参数' % self.field_name)
+            raise ParamsValidationError('param integrity error, field %s is required' % self.field_name)
 
     def type_validate(self):
-        """参数类型验证"""
+        """Parameter type validation"""
         if self.required and not isinstance(self.field_value, self.field_type):
-            raise ParamsValidationError('参数错误, 字段%s需要是%s类型' % (self.field_name, self.field_type_name))
+            raise ParamsValidationError('param type error, field %s should be %s type' % (
+                self.field_name, self.field_type_name))
 
     def length_validate(self):
         """参数长度验证"""
         if self.max_length and len(self.field_value) > self.max_length:
-            raise ParamsValidationError('字段%s长度不能超过%s' % (self.field_name, self.max_length))
+            raise ParamsValidationError('param length error, field %s length cannot exceed %s' % (
+                self.field_name, self.max_length))
 
         if self.min_length:
             if len(self.field_value) < self.min_length:
-                raise ParamsValidationError('字段%s长度不能小于%s' % (self.field_name, self.min_length))
+                raise ParamsValidationError('param length error, field %s length cannot be less than%s' % (
+                    self.field_name, self.min_length))
 
     def validate(self):
         self.integrity_validate()
         self.type_validate()
         self.length_validate()
-        if not self.field_value and not self.required and self.default:
+        if not self.field_value and not self.required and self.default is not None:
             self.field_value = self.default
 
     @property
@@ -94,9 +97,13 @@ class DateTimeField(BaseField):
     def type_validate(self):
         try:
             if not isinstance(dt_str_to_datetime(self.field_value), self.field_type):
-                raise ParamsValidationError('参数错误, 字段%s需要是%s类型' % (self.field_name, self.field_type_name))
+                raise ParamsValidationError('param type error, field %s should be %s type' % (
+                    self.field_name, self.field_type_name
+                ))
         except:
-            raise ParamsValidationError('参数错误, 字段%s需要是%s类型' % (self.field_name, self.field_type_name))
+            raise ParamsValidationError('param type error, field %s should be %s type' % (
+                self.field_name, self.field_type_name
+            ))
         self.field_value = dt_str_to_datetime(self.field_value)
 
 
